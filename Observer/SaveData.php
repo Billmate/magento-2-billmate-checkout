76bc1cf51a0feb3da7117ea56e1cddc0ab5433c3
alex <a.grigorenko.belvg@gmail.com>
2020-05-22 12:33:54 +0300

    M2-113 [BUGFIX] excluded saving comments  if comments disabled

--- Observer/SaveData.php
+++ Observer/SaveData.php
@@ -5,23 +5,63 @@ namespace Billmate\BillmateCheckout\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Payment\Observer\AbstractDataAssignObserver;
 
-class SaveData extends AbstractDataAssignObserver{
+/**
+ * Class SaveData
+ * @package Billmate\BillmateCheckout\Observer
+ */
+class SaveData extends AbstractDataAssignObserver
+{
 
+    /**
+     * @var \Magento\Sales\Model\Order
+     */
     protected $order;
+
+    /**
+     * @var \Magento\Quote\Model\Quote
+     */
     protected $quote;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
     protected $checkoutSession;
 
+    /**
+     * @var \Billmate\BillmateCheckout\Helper\Config
+     */
+    private $configHelper;
+
+    /**
+     * SaveData constructor.
+     *
+     * @param \Magento\Sales\Model\Order               $order
+     * @param \Magento\Quote\Model\Quote               $quote
+     * @param \Magento\Checkout\Model\Session          $_checkoutSession
+     * @param \Billmate\BillmateCheckout\Helper\Config $configHelper
+     */
     public function __construct(
         \Magento\Sales\Model\Order $order,
         \Magento\Quote\Model\Quote $quote,
-        \Magento\Checkout\Model\Session $_checkoutSession
+        \Magento\Checkout\Model\Session $_checkoutSession,
+        \Billmate\BillmateCheckout\Helper\Config $configHelper
     ){
         $this->order = $order;
         $this->quote = $quote;
         $this->checkoutSession = $_checkoutSession;
+        $this->configHelper = $configHelper;
     }
 
-    public function execute(Observer $observer){
+    /**
+     * @param Observer $observer
+     *
+     * @throws \Exception
+     */
+    public function execute(Observer $observer)
+    {
+        if (!$this->configHelper->isCommentsEnabled()) {
+            return;
+        }
         $orderIds = $observer->getEvent()->getOrderIds();
         if (empty($orderIds) || !is_array($orderIds) || $this->checkoutSession->getData('has_saved_comment') == 1) {
             return;
